474ecd8353cc117138357613ac32d6e325951443
DESKTOP-8VA6U7V\ASUS <ltv.leo297@gmail.com>
2022-04-17 02:33:47 +0700

    fix error view bagger swipe

--- app/src/main/java/mobile_project/music_app/Activity/MainActivity.java
+++ app/src/main/java/mobile_project/music_app/Activity/MainActivity.java
@@ -20,34 +20,34 @@ public class MainActivity extends AppCompatActivity {
     ViewPager2 viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.fragment_trangchu);
 //        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        anhxa();
-//        init();
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        setContentView(R.layout.fragment_trangchu);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        anhxa();
+        init();
     }
 
-//    private void init(){
-//        FragmentManager fm = getSupportFragmentManager();
-//        MainViewPagerAdapter mainViewPagerAdapter = new MainViewPagerAdapter(fm,getLifecycle());
-//        mainViewPagerAdapter.addFragment(new Fragment_TrangChu(),"Trang chu");
-//        mainViewPagerAdapter.addFragment(new Fragment_TimKiem(),"Tim kiem");
-//        viewPager.setAdapter(mainViewPagerAdapter);
-//
-//
-//        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager, true, new TabLayoutMediator.OnConfigureTabCallback() {
-//            @Override
-//            public void onConfigureTab(TabLayout.Tab tab, int position) {
-//                if(position==0)
-//                    tab.setIcon(R.drawable.icontrangchu);
-//                if(position==1)
-//                    tab.setIcon(R.drawable.icontimkiem);
-//            }
-//        });
-//        tabLayoutMediator.attach();
-//    }
+    private void init(){
+        FragmentManager fm = getSupportFragmentManager();
+        MainViewPagerAdapter mainViewPagerAdapter = new MainViewPagerAdapter(fm,getLifecycle());
+        mainViewPagerAdapter.addFragment(new Fragment_TrangChu(),"Trang chu");
+        mainViewPagerAdapter.addFragment(new Fragment_TimKiem(),"Tim kiem");
+        viewPager.setAdapter(mainViewPagerAdapter);
+        viewPager.setUserInputEnabled(false);
+//        viewPager. setRotationY(180);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager, true, new TabLayoutMediator.OnConfigureTabCallback() {
+            @Override
+            public void onConfigureTab(TabLayout.Tab tab, int position) {
+                if(position==0)
+                    tab.setIcon(R.drawable.icontrangchu);
+                if(position==1)
+                    tab.setIcon(R.drawable.icontimkiem);
+            }
+        });
+        tabLayoutMediator.attach();
+    }
     private void anhxa() {
         tabLayout= findViewById(R.id.myTabLayout);
         viewPager= findViewById(R.id.myViewPager);
