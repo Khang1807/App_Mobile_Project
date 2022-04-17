24c35b83c9c7e1c023ba1000664ebe78b04cea66
LeeTuanKhanh <19521681@gm.uit.edu.vn>
2022-04-17 22:15:42 +0700

    add, update, fix frag_lib, frag_user

---
+++ app/src/main/java/mobile_project/music_app/Fragment/Fragment_Lib_NgheSi.java
@@ -0,0 +1,15 @@
+package mobile_project.music_app.Fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import mobile_project.music_app.R;
+
+public class Fragment_Lib_NgheSi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_lib_nghesi);
+    }
+}
--- app/src/main/java/mobile_project/music_app/Fragment/Fragment_Libraries.java
+++ app/src/main/java/mobile_project/music_app/Fragment/Fragment_Libraries.java
@@ -8,15 +8,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.squareup.picasso.Picasso;
 
 import mobile_project.music_app.R;
 
 public class Fragment_Libraries extends Fragment {
     View view;
+    ViewPager viewPager;
+
     @Nullable
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view= inflater.inflate(R.layout.fragment_libaries,container,false);
+        view = inflater.inflate(R.layout.fragment_libaries, container, false);
         return view;
     }
 }
+
