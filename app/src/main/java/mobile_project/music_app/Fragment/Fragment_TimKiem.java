e7fc950ee52450b6d94d12a8813f99283d6063e9
LeeTuanKhanh <19521681@gm.uit.edu.vn>
2022-04-17 12:56:11 +0700

    update, add, fix from fragment_timkiem, compelte screen for timkiem

--- app/src/main/java/mobile_project/music_app/Fragment/Fragment_TimKiem.java
+++ app/src/main/java/mobile_project/music_app/Fragment/Fragment_TimKiem.java
@@ -1,22 +1,44 @@
 package mobile_project.music_app.Fragment;
-
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+//import mobile_project.music_app.Adapter.TimKiemAdapter;
 import mobile_project.music_app.R;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fragment_TimKiem extends Fragment {
+
     View view;
+    androidx.appcompat.widget.Toolbar toolbar;
+    RecyclerView recyclerViewtim;
+    TextView textViewnull;
+//
+
     @Nullable
-    //Gan view cua fragment_timkiem cho file java
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,@Nullable  Bundle savedInstanceState) {
-        view= inflater.inflate(R.layout.fragment_timkiem,container,false);
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_timkiem, container, false);
+//        toolbar = view.findViewById(R.id.toilbartimkiem);
+//        recyclerViewtim = view.findViewById(R.id.recyclerviewtimkiem);
+//        textViewnull = view.findViewById(R.id.textviewtimkiemnull);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
         return view;
     }
 }
