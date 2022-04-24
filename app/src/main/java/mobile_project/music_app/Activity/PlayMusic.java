871929ac3f2b84b1773e3cbdf4d9a592997fe2a5
c1kk <19521694@gm.uit.edu.vn>
2022-04-24 20:19:09 +0700

    ok

---
+++ app/src/main/java/mobile_project/music_app/Activity/PlayMusic.java
@@ -0,0 +1,14 @@
+package mobile_project.music_app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayMusic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_music);
+    }
+}
