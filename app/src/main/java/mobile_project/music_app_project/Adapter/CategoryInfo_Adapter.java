453cb3a862222c02ce1249e3084d6e4412730ff6
khang ho <19521661.gm.uit.edu.vn>
2022-06-06 22:27:03 +0700

    Update home screen, new activity

---
+++ app/src/main/java/mobile_project/music_app_project/Adapter/CategoryInfo_Adapter.java
@@ -0,0 +1,82 @@
+package mobile_project.music_app_project.Adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import mobile_project.music_app_project.Activity.Category_Info_Activity;
+import mobile_project.music_app_project.Model.ModelBaiHat;
+import mobile_project.music_app_project.R;
+
+public class CategoryInfo_Adapter extends RecyclerView.Adapter<CategoryInfo_Adapter.ViewHolder> {
+    Context context;
+    ArrayList<ModelBaiHat> data;
+
+    public CategoryInfo_Adapter(Context context, ArrayList<ModelBaiHat> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.category_music_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ModelBaiHat baihat = data.get(position);
+        holder.category_song.setText(baihat.getMusicName());
+        String name = baihat.getImgUrl();
+        int resID  =context.getResources().getIdentifier(name, "drawable", context.getPackageName());
+        holder.song_img.setImageResource(resID);
+
+        Log.i(data.get(position).getCategoryId(),"MA");
+//            holder.divCategory_Music.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Log.i(data.get(positionOfData).getMusicId(),"abc");
+//                    GetDataCategory(data.get(positionOfData).getMusicId());
+//                }
+//            });
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView category_song;
+        ImageView song_img;
+        LinearLayout divCategory_Music;
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            category_song = view.findViewById(R.id.category_song_name);
+            song_img = view.findViewById(R.id.imgMusic_Category);
+            divCategory_Music = view.findViewById(R.id.divCategory_Music);
+        }
+    }
+
+
+}
--- app/src/main/java/mobile_project/music_app_project/Fragment/Fragment_Libraries.java
+++ app/src/main/java/mobile_project/music_app_project/Fragment/Fragment_Libraries.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +18,7 @@ import com.google.android.material.tabs.TabLayout;
 import de.hdodenhof.circleimageview.CircleImageView;
 import mobile_project.music_app_project.Activity.MainActivity;
 import mobile_project.music_app_project.Adapter.ViewPagerThuVien;
+import mobile_project.music_app_project.Model.ModelTheLoai;
 import mobile_project.music_app_project.R;
 
 
@@ -25,6 +27,7 @@ public class Fragment_Libraries extends Fragment {
     ViewPager viewPager;
     ImageView imgAddThuVien;
     CircleImageView imguser;
+    ModelTheLoai category;
     ProgressDialog progressDialog;
     private String tenThuVien;
     private MainActivity hm;
@@ -33,16 +36,17 @@ public class Fragment_Libraries extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_libaries, container, false);
+
         AnhXa();
         init();
+
 //        imgAddThuVien.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
 //                openDialog();
 //            }
 //        });
-        return  view;
+        return inflater.inflate(R.layout.fragment_libaries, container, false);
     }
 
     private void init() {
@@ -53,6 +57,10 @@ public class Fragment_Libraries extends Fragment {
         tabLayout.setupWithViewPager(viewPager);
 //        Picasso.get().load(hm.getUrl()).into(imguser);
     }
+
+
+
+
     private void AnhXa() {
         hm = (MainActivity) getActivity();
         tabLayout = view.findViewById(R.id.tabLayouttv);
